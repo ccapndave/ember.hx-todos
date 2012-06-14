656ab14520bcd38da1d8779f85d67602c818f673
Dave Keen <dev@ruffness.com>
2012-06-14 12:08:24 +0200

    Working todo example!

--- src/todos/TodosController.hx
+++ src/todos/TodosController.hx
@@ -5,61 +5,44 @@ using ember.ArrayExtensions;
 
 class TodosController extends ArrayController<Todo> {
 
-    override public function init() {
-        content = [];
+	override public function init() {
+		content = [];
 
-        super.init();
-    }
-
-    public function createTodo(title:String) {
-        var todo = new Todo();
-        todo.title = title;
-
-        content.pushObject(todo);
-    }
-
-    public function clearCompletedTodos() {
-        content.filter(function(todo) { return todo.isDone; } ).foreach(content.removeObject);
-    }
-
-    @:property("@each.isDone")
-    public function remaining():Int {
-        return content.filter(function(todo) { return todo.isDone; } ).length;
-    }
-
-    @:property("length")
-    public function isEmpty():Bool {
-        return content.length == 0;
-    }
-
-    @:property("@each.isDone")
-    public function allAreDone(?key:String, ?value:Bool):Bool {
-        return false;
-    }
-
-}
-
-/*
-clearCompletedTodos: function() {
-	this.filterProperty('isDone', true).forEach(this.removeObject, this);
-  },
-}
-
-  remaining: function() {
-	return this.filterProperty('isDone', false).get('length');
-  }.property('@each.isDone'),
-
-  isEmpty: function() {
-	return this.get('length') === 0;
-  }.property('length'),
-
-  allAreDone: function(key, value) {
-	if (arguments.length === 2) {
-	  this.setEach('isDone', value);
-
-	  return value;
-	} else {
-	  return !this.get('isEmpty') && this.everyProperty('isDone', true);
+		super.init();
 	}
-  }.property('@each.isDone')
-*/
+
+	public function createTodo(title:String) {
+		var todo = new Todo();
+		todo.title = title;
+
+		content.pushObject(todo);
+	}
+
+	public function clearCompletedTodos() {
+		content.filter(function(todo) { return todo.isDone; }).foreach(content.removeObject);
+	}
+
+	@:property("@each.isDone")
+	public function remaining():Int {
+		return content.filter(function(todo) { return !todo.isDone; }).length;
+	}
+
+	@:property("length")
+	public function isEmpty():Bool {
+		return content.length == 0;
+	}
+
+	@:property("@each.isDone")
+	public function allAreDone(?key:String, ?value:Bool):Bool {
+		return
+			switch (value) {
+				case null:
+					// If value is null then the caller is trying to retrieve whether or not all are done
+					content.length > 0 && content.foreach(function(todo) { return todo.isDone; });
+				default:
+					// If value is true or false then the caller is trying to set whether or not all are done
+					content.foreach(function(todo) { return todo.isDone = value; });
+			};
+	}
+
+}
